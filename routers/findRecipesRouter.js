2e44a7e637e2925f985fb6091fd9f19156a6209a
Unknown <opticalfalicy@gmail.com>
2019-02-06 15:26:09 -0600

    Cleaning code

--- routers/findRecipesRouter.js
+++ routers/findRecipesRouter.js
@@ -5,8 +5,6 @@ const Recipe = require("../Schemas/recipeSchema");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  // const item = req.body;
-  // console.log('')
   Recipe.find({})
     .populate("recipes")
     .then(recipes => {
@@ -18,17 +16,5 @@ router.route("/").get((req, res) => {
     })
     .catch(error => res.status(500).json(`Error from server: $(error)`));
 });
-
-// app.get("/api/recipes", function(req, res) {
-//   console.log("fetching recipes");
-
-//   // use mongoose to get all recipes in the database
-//   Recipe.find(function(err, recipes) {
-//     // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-//     if (err) res.send(err);
-
-//     res.json(recipes); // return all recipes in JSON format
-//   });
-// });
 
 module.exports = router;
